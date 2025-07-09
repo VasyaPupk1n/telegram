69e5ae9b331e73dc3f20bb01cd3877f4945aac41
feadiesa <feadiesa@gmail.com>
2025-07-10 01:08:15 +0300

    test

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import Three from "./Test/Three";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <Three />
   </React.StrictMode>
 )
